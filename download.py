62e29c6917e8cc871c4c41b81cec97441d64be9b
TheWhale01 <ard.rasp01@gmail.com>
2023-10-04 14:01:22 +0200

    headless mode does not work with chrome

--- download.py
+++ download.py
@@ -11,22 +11,22 @@ def get_links(filename: str()) -> list(str()):
 				links.append(line.replace('\n', ''))
 	return (links)
 
-def downloading_file(directory: str()) -> bool():
+def is_part_file(directory: str()) -> bool():
 	for file in os.listdir(directory):
-		if (file.endswith('.crdownload')):
+		if (file.endswith('.part')):
 			return (True)
 	return (False)
 
-def init(download_dir: str()) -> webdriver.chrome.webdriver.ChromiumDriver:
-	options = webdriver.ChromeOptions()
-	options.binary_location = '/bin/'
-	# options.add_argument('--headless')
-	options.add_experimental_option("prefs", {
-		'download.default_directory': download_dir,
-	})
-	extension_path = os.path.abspath('./cjpalhdlnbpafiamejdnhcphjbkeiagm.crx')
-	options.add_extension(extension_path)
-	browser = webdriver.Chrome(options=options)
+def init(download_dir: str()) -> webdriver.firefox.webdriver.WebDriver:
+	options = webdriver.FirefoxOptions()
+	options.set_preference('browser.download.folderList', 2)
+	options.set_preference('browser.download.manager.showWhenStarting', False)
+	options.set_preference('browser.download.dir', download_dir)
+	options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
+	options.add_argument('--headless')
+	extension_path = os.path.abspath('./ublock_origin-1.52.2.xpi')
+	browser = webdriver.Firefox(options=options)
+	browser.install_addon(extension_path)
 	sleep(5)
 	return (browser)
 
@@ -37,24 +37,26 @@ def main():
 	browser = init(download_dir)
 	links = get_links('links.txt')
 	for link in links:
+		print('Link accessed.')
 		browser.get(link)
 		sleep(5)
+		browser.find_element(By.CLASS_NAME, 'cookie_box_close').click()
 		browser.find_element(By.XPATH, '/html/body/div[5]/div[1]/button').click()
-		sleep(4)
+		sleep(1)
 		access_button = browser.find_element(By.ID, 'dlb')
-		print("Waiting for access button to be clickable")
+		print('Waiting for access button.')
 		while (True):
 			try:
 				access_button.click()
 				break
 			except:
 				sleep(60)
-		browser.find_element(By.XPATH, "/html/body/div[1]/a[2]").click()
-		sleep(1)
 		browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/a').click()
-		print('Downloading file.')
-		while (downloading_file(download_dir)):
+        sleep(5)
+		print('download started.')
+		while (is_part_file(download_dir)):
 			sleep(60)
+	browser.close()
 
 if (__name__ == '__main__'):
 	main()
---
+++ readme.md
@@ -0,0 +1,11 @@
+# 1Ficher downloader
+
+## Requirements
+ - Firefox (apt)
+ - python3
+
+# How to use it ?
+1. create a links.txt file.
+2. paste the 1fichier links inside
+3. run `python3 download.py`
+4. your downloads should be in the ${program's dir}/downloads
