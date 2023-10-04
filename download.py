0e2ada935c4f65981578b9a2abce1539f4b0fa93
TheWhale01 <ard.rasp01@gmail.com>
2023-10-04 22:19:03 +0200

    checking for wait time on the page

--- download.py
+++ download.py
@@ -18,15 +18,30 @@ def is_part_file(directory: str()) -> bool():
 			return (True)
 	return (False)
 
+def check_waiting_time(browser):
+	try:
+		time_element = browser.find_element(By.CLASS_NAME, 'ct_warn')
+		time = time_element.get_attribute('innerText')
+		time = time.split('\n')[2]
+		time = time.replace('Vous devez attendre encore ', '')
+		time = time.replace(' minutes.', '')
+		time = int(time)
+		return (time * 3600)
+	except:
+		return (0)
+	return (0)
+
 def init(download_dir: str()) -> webdriver.firefox.webdriver.WebDriver:
 	options = webdriver.FirefoxOptions()
 	options.set_preference('browser.download.folderList', 2)
 	options.set_preference('browser.download.manager.showWhenStarting', False)
 	options.set_preference('browser.download.dir', download_dir)
 	options.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/octet-stream')
-	options.add_argument('--headless')
+	# options.add_argument('--headless')
 	extension_path = os.path.abspath('./ublock_origin-1.52.2.xpi')
 	browser = webdriver.Firefox(options=options)
+	browser.set_window_position(0, 0)
+	browser.set_window_size(1260, 720)
 	browser.install_addon(extension_path)
 	sleep(5)
 	return (browser)
@@ -46,17 +61,17 @@ def main():
 		except:
 			pass
 		browser.find_element(By.XPATH, '/html/body/div[5]/div[1]/button').click()
-		sleep(1)
+		time = check_waiting_time(browser)
+		if (time):
+			print(f'waiting for {time / 3600} minutes.')
+			wait_btn = browser.find_element(By.ID, 'dlw')
+			browser.execute_script('arguments[0].scrollIntoView();', wait_btn)
+			sleep(time)
 		access_button = browser.find_element(By.ID, 'dlb')
-		print('Waiting for access button.')
-		while (True):
-			try:
-				access_button.click()
-				break
-			except Exception as e:
-				print(e, file=sys.stderr)
-				sleep(60)
-				access_button = browser.find_element(By.ID, 'dlb')
+		browser.execute_script('arguments[0].scrollIntoView();', access_button)
+		if (not access_button.is_displayed()):
+			sleep(60)
+		access_button.click()
 		browser.find_element(By.XPATH, '/html/body/div[4]/div[2]/a').click()
 		sleep(5)
 		print('download started.')
